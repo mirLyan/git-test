830d1c28640949bec55bc5032bc9dda44642ebb8
Lyanyan <978225953@qq.com>
2021-10-01 22:07:17 +0800

    second commit

--- src/main/java/com/yyds/git/GitTest.java
+++ src/main/java/com/yyds/git/GitTest.java
@@ -7,5 +7,6 @@ package com.yyds.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("hellogit2");
     }
 }
